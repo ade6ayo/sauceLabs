a168645c4dbbc5d084116924d8dd54e07f019b9d
ade6ayo <adetunji003@gmail.com>
2024-07-10 04:44:38 +0100

    specflow

---
+++ specflowpw/SpecflowAppSauceLabs/SpecflowAppSauceLabs/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace SpecflowAppSauceLabs.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ specflowpw/SpecflowAppSauceLabs/SpecflowAppSauceLabs/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowAppSauceLabs.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
